d7c7cc91594824e5bcb48770dde7aa2482bba1cf
estevanmaito <ejmaito@gmail.com>
2016-01-12 09:16:30 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#File Metadata Microservice API
+A simple API to retrieve the size of the file uploaded.
---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var multer  = require('multer');
+var upload = multer();
+
+var app = express();
+
+app.get('/', function(req, res) {
+	
+	res.sendFile(process.cwd() + '/index.html');
+});
+
+// upload.single(THIS SHOULD BE THE SAME AS THE INPUT'S NAME)
+app.post('/file-size', upload.single('file'), function(req,res){
+
+    res.json(req.file.size);
+});
+
+app.listen(process.env.PORT || 3000);
