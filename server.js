f85af81bdd6fa6beb4e974e78a2361790f7b0a5f
Hola Nguyen <kevinhoa95@gmail.com>
2017-06-07 09:53:32 +0000

    initial pr

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ var upload = multer();
 var app = express();
 
 app.get('/', function(req, res) {
-	
 	res.sendFile(process.cwd() + '/index.html');
 });
 
 app.post('/file-size', upload.single('file'), function(req,res){
-
     res.json(req.file.size);
 });
 
